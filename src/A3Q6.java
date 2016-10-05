4c96dc10cc945e20765a791df7909704e8425242
watsk8668 <watsk8668@HRH-IC0027335.wrdsb.ca>
2016-10-05 13:18:16 -0400

    1 done

--- src/A3Q6.java
+++ src/A3Q6.java
@@ -12,7 +12,15 @@ public class A3Q6 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // 
+    public void insertionSort(int[] n) {
+        for (int i = 1; i < n.length; i++) {
+            int j = i;
+            while (j > 0 && n[j - 1] > n[j]) {
+                int temp = n[j];
+                n[j] = n[j - 1];
+                n[j - 1] = temp;
+                j--;
+            }
+        }
     }
 }
