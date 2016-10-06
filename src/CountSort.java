679ee8614bd00fe46b5dd84abe536c3f1688b106
hadik9595 <hadik9595@HRH-IC0027320.wrdsb.ca>
2016-10-06 15:59:24 -0400

    Hmm thinking

--- src/CountSort.java
+++ src/CountSort.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -14,13 +17,29 @@ public class CountSort {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] n = {56,72,26,56,43,72,0};
+        int[] n = {56,1,56,3,43,72,0};
+        CountSort test = new CountSort();
         
-        
+        for (int i = 0; i < n.length; i++) {
+              test.countSort(n);
+        System.out.println(n[i]);
+        }
+      
         
     }
     public void countSort(int[]n){
+        int[]tracker = new int[101];
+        int keeper = 0;
         for (int i = 0; i < n.length; i++) {
+            
+            int number = n[i];
+            int number2 = tracker[number];
+         
+             tracker[number] = keeper + 1;
+            
+             System.out.println(tracker[56]);
+            
+           
             
         }
         
