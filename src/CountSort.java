4a6467c773beac29b59f1742389b99460097b9a7
hadik9595 <hadik9595@HRH-IC0027320.wrdsb.ca>
2016-10-06 14:23:00 -0400

    Victor Guanine

---
+++ src/CountSort.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hadik9595
+ */
+public class CountSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] n = {56,72,26,56,43,72,0};
+        
+        
+        
+    }
+    public void countSort(int[]n){
+        for (int i = 0; i < n.length; i++) {
+            
+        }
+        
+    }
+}
