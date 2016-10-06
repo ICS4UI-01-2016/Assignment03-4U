ba248001ef0b6c648c6d58df6fbb8d682f6d132e
tatad6701 <tatad6701@HRH-IC0027318.wrdsb.ca>
2016-10-06 14:24:02 -0400

    (Fifth Commit)

--- src/ProgrammingExcercises.java
+++ src/ProgrammingExcercises.java
@@ -9,13 +9,25 @@
  */
 public class ProgrammingExcercises {
 
+    public void insertionSort(int[] n) {
+        for (int i; 0 < n.length; i++) {
+            int j = i;
+            // While loop = if the number is greater than 0 and the number before that specific number is less than J
+            // Swtich the integers!
+            while (j > 0 && n[j - 1] < n[j]) {
+                int temp = n[j];
+                n[j] = n[j - 1];
+                n[j - 1] = temp;
+                j--;
+            }
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         // A3Q6
-    
-    public 
-}
+    }
 }
