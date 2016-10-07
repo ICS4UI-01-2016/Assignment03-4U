0102414416abb359aedaa9821f94bf31472edee8
guanv6321 <guanv6321@HRH-IC0027321.wrdsb.ca>
2016-10-07 13:18:24 -0400

    q6

---
+++ src/A3Q6.java
@@ -0,0 +1,42 @@
+
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guanv6321
+ */
+public class A3Q6 {
+
+    public static void insertionSort(int[]n){
+      for(int i = 1; i < n.length; i++)
+   {
+      int j = i;
+      while(j>0 && n[j] > n[j-1])
+      {
+         int temp = n[j];
+         n[j] = n[j-1];
+         n[j-1] = temp;
+         j--;
+      }
+   }
+        System.out.println(n);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A3Q6 test = new A3Q6();
+        int[] n = new int[4];
+        n[0] = 3;
+        n[1] = 9;
+        n[2] = 4; 
+        n[3] = 15;
+        
+    }
+}
