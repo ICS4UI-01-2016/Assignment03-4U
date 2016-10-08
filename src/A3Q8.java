2813a2fde31db088fafc311031f01c971f622596
Hadida Family <Hadida Family@192.168.2.38>
2016-10-08 16:13:13 -0400

    Onto q8

---
+++ src/A3Q8.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hadida Family
+ */
+public class A3Q8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] words = {"Banish","Zoey","Khaled","Victor","Arshbarsh","Majed"};
+        int result = words[0].compareTo(words[4]);
+        System.out.println(result);
+        
+    }
+    public void insertionSort(String[] letters){
+   for(int i = 1; i < letters.length; i++)
+   {
+      String words = letters[i];
+      
+   }
+}
+    
+}
--- src/CountSort.java
+++ src/CountSort.java
@@ -3,7 +3,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author hadik9595
@@ -15,39 +14,55 @@ public class CountSort {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] n = {56,65,44,65,98,44};
+        //the list used for testing
+        int[] n = {23, 65, 1, 65, 98, 1, 65, 0, 23, 0, 99, 54, 23, 75, 34, 75, 87, 34, 23, 88, 11, 64, 23, 75, 35, 72};
+        //used for method calling 
         CountSort test = new CountSort();
-        
-     
-              test.countSort(n);
-        
-        
-      
-        
-    }
-    public void countSort(int[]n){
-        int[]tracker = new int[101];
-        
+
+        //first calls the method then with a for loop, prints out the sorted array
+        test.countSort(n);
+        System.out.print("The sorted list: ");
         for (int i = 0; i < n.length; i++) {
-            
-            int number = n[i];
-            int number2 = tracker[number];
-           
-            
-            
-             for (int j = 0; j < 1; j++) {
-             tracker[number]++;
+            //ASK LAMONT if it's ok to have a COMMA being printed at the end of the array. ALSO ASK IF WE ANSWER QUESTION
+            System.out.print(n[i] + ",");
         }
-             
-            }
-        
-        
-        for (int j = 0; j < tracker.length; j++) {
-            if(tracker[j] != 0){
-                 System.out.println("Number " + j + " and " + tracker[j]);
-                 
-            }
+        //separates the BUILD SUCCESSFUL with the list 
+        System.out.println("");
+
     }
-    
-}
+
+    //a void method that doesn't return a value, used to sort our list
+    public void countSort(int[] n) {
+        //a tracker array that saves the amount of repeated specific values from 0-100 within the list 
+        int[] tracker = new int[101];
+        //an integer named keeper that is used to move from one slot to another during sorting 
+        int keeper = 0;
+        //a for loop that goes through list 
+        for (int i = 0; i < n.length; i++) {
+            //assigns number to each spot 
+            int number = n[i];
+            //adds one everytime it comes across a number within the list, essentially our counter
+            tracker[number]++;
+
+        }
+
+        //a for loop same as one above, goes through the entire list of our tracker (including 0s)
+        for (int j = 0; j < tracker.length; j++) {
+            //an integer that stores a number from the list
+            int looper = tracker[j];
+
+            //only looks at numbers that exist within the list (meaning no 0s would be read)
+            if (tracker[j] != 0) {
+                //a for loop that is used to sort MULTIPLES of the same number accordingly
+                for (int i = 0; i < looper; i++) {
+                    //assigns an array the sorted number
+                    n[keeper] = j;
+                    //counts up, meaning moves on to the next slot (spot) in the list
+                    keeper++;
+                }
+            }
+
+        }
+
+    }
 }
