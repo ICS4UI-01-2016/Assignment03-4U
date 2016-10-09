a036a2b8c3cfab0c42d4e54e0b6c1934b366c992
Hadida Family <Hadida Family@192.168.2.38>
2016-10-09 17:54:33 -0400

    Annnnnd almost done except for a clarification on Q4

--- src/A3Q8.java
+++ src/A3Q8.java
@@ -15,17 +15,42 @@ public class A3Q8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String[] words = {"Banish","Zoey","Khaled","Victor","Arshbarsh","Majed"};
-        int result = words[0].compareTo(words[4]);
-        System.out.println(result);
-        
+        //the list of strings
+        String[] words = {"Exodia", "Kevin", "Khaled", "Victor", "Arshbarsh", "Majed","Lamont"};
+
+        //necessary for calling a method later
+        A3Q8 test = new A3Q8();
+        System.out.print("The sorted list of strings: ");
+        //a for loop that prints out the sorted string list
+        for (int i = 0; i < words.length; i++) {
+            //calls the method
+            test.insertionSort(words);
+            System.out.print(words[i]+ ",");
+        }
+        //separates the list of strings from BUILD SUCCESSFUL
+        System.out.println("");
     }
-    public void insertionSort(String[] letters){
-   for(int i = 1; i < letters.length; i++)
-   {
-      String words = letters[i];
-      
-   }
-}
-    
+
+    //the method that sorts the strings all accordingly
+    public void insertionSort(String[] words) {
+        //a for loop that is needed assign word to each spot through conversion of j and i
+        for (int i = 1; i < words.length; i++) {
+            //a variable that takes the same value as i so that i is not affected by the for loop below
+            int j = i;
+            
+            //a while loop that operates until j is 0 (exhausted), compares the string before and the current string
+            while (j > 0 && words[j - 1].compareTo(words[j]) > words[j].compareTo(words[j])) {
+                //stores the current string as temporary
+                String temp = words[j];
+                //if the first letter of the word comes before the first letter of second word, swap
+                words[j] = words[j - 1];
+                //same thing as before, instead this time it swaps with temp because the word is saved 
+                words[j - 1] = temp;
+                //j is minused overtime, meaning it's going through every spot
+                j--;
+            }
+
+        }
+    }
+
 }
--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -15,39 +15,39 @@ public class InsertionSort {
     public static void main(String[] args) {
         // TODO code application logic here
         //Set of numbers 
-         int n[] = new int[5];
-         n[0] = 0;
-         n[1] = 2;
-         n[2] = 43;
-         n[3] = 5;
-         n[4] = 77;
-     
-         //initializer for calling a method 
-         InsertionSort test = new InsertionSort();
-         
-         // a loop that spits out all numbers sorted 
-         for(int i = 0; i < n.length; i++){
-             //calls the method
-         test.insertionsSort(n);
-         //prints out all the numbers
-             System.out.println(n[i]);
-         }
+        int n[] = {3, 4, 6, 7, 8, 1, 9, 2};
+
+        //initializer for calling a method 
+        InsertionSort test = new InsertionSort();
+
+        // a loop that spits out all numbers sorted 
+        for (int i = 0; i < n.length; i++) {
+            //calls the method
+            test.insertionsSort(n);
+            //prints out all the numbers
+            System.out.println(n[i]);
+        }
     }
-    
+
     //insertionSort method 
-    public void insertionsSort(int[]n){
-         for(int i = 1; i < n.length; i++)
-   {
-      int j = i;
-      while(j>0 && n[j-1] < n[j])
-      {
-         int temp = n[j];
-         n[j] = n[j-1];
-         n[j-1] = temp;
-         j--;
-         
-      }
-   }
-        
+    public void insertionsSort(int[] n) {
+        //a for loop that is needed assign a number to each spot through conversion of int i to j
+        for (int i = 1; i < n.length; i++) {
+            //a variable that takes the same value as i so that i is not affected by the for loop below, it's also what changes spot
+            int j = i;
+            //a while loop designed so that it compares the current number to the previous number, then stops operating when j is exhausted
+            while (j > 0 && n[j - 1] < n[j]) {
+                //stores the current number as temporary
+                int temp = n[j];
+                //if the first number is bigger than second number, swap them 
+                n[j] = n[j - 1];
+                //same thing, except this first number takes the big number saved as a temp (to avoid mis swaping)
+                n[j - 1] = temp;
+                //goes down the list of numbers
+                j--;
+
+            }
+        }
+
     }
 }
