9ac9286a5f8b9fdb6bfbaf6cbb4eb756aab1daa6
guanv6321 <guanv6321@HRH-IC0027321.wrdsb.ca>
2016-10-11 13:16:48 -0400

    q7

--- src/A3Q6.java
+++ src/A3Q6.java
@@ -1,31 +1,27 @@
 
-
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author guanv6321
  */
 public class A3Q6 {
 
-    public static void insertionSort(int[]n){
-      for(int i = 1; i < n.length; i++)
-   {
-      int j = i;
-      while(j>0 && n[j] > n[j-1])
-      {
-         int temp = n[j];
-         n[j] = n[j-1];
-         n[j-1] = temp;
-         j--;
-      }
-   }
-        System.out.println(n);
+    public static void insertionSort(int[] n) {
+        for (int i = 1; i < n.length; i++) {
+            int j = i;
+            while (j > 0 && n[j] > n[j - 1]) {
+                int temp = n[j];
+                n[j] = n[j - 1];
+                n[j - 1] = temp;
+                j--;
+            }
+        }
+
     }
+
     /**
      * @param args the command line arguments
      */
@@ -35,8 +31,11 @@ public class A3Q6 {
         int[] n = new int[4];
         n[0] = 3;
         n[1] = 9;
-        n[2] = 4; 
-        n[3] = 15;
-        
+        n[2] = 4;
+        n[3] = 2;
+        test.insertionSort(n);
+        for(int i = 0; i < n.length; i++){
+            System.out.println(n[i]);
+        }
     }
 }
---
+++ src/A3Q7.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guanv6321
+ */
+public class A3Q7 {
+
+    public static void countingSort(int[] n){
+        
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A3Q7 test = new A3Q7();
+        int[] n = new int[4];
+        n[0] = 5;
+        n[1] = 3;
+        n[2] = 8;
+        n[3] = 10;
+    }
+}
