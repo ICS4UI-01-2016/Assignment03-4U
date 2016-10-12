beab477d90cd5d1c66e939b3a9e16ee5f1b1ca90
richj0985 <richj0985@HRH-IC0027334.wrdsb.ca>
2016-10-12 14:22:34 -0400

    All Done - Still Need to Do Last Comments and Changes

--- src/A3Q6.java
+++ src/A3Q6.java
@@ -12,6 +12,8 @@ public class A3Q6 {
      * @param args the command line arguments
      */
     
+    // Method for the sorting method of Insertion Sort
+    // 
     public void insertionSort(int[] n){
        for(int i = 1; i < n.length; i++)
        {
@@ -26,10 +28,19 @@ public class A3Q6 {
        }
     }
     
+    // Main method that inputs a series of numbers from an array to the method
+    // and outputs the ordered array of numbers
     public static void main(String[] args) {
+        // Tests file
         A3Q6 test = new A3Q6();
+        
+        // A randomized array of numbers out of ourder
         int [] n = {10, 8, 4, 9, 1, 3, 2, 7, 5, 6};
+        
+        // call on the method of insertion sort with the perimeter as the array of n
         test.insertionSort(n);
+        
+        // output the sorted array of n
         for(int i = 0; i < n.length; i ++){
             System.out.print(n[i] + " ");
         }
