e85fd843678558aab74474faf578f58fd0286ff1
guanv_000 <guanv_000@Amy-PC>
2016-10-12 17:35:48 -0400

    finished q7

--- src/A3Q7.java
+++ src/A3Q7.java
@@ -10,7 +10,18 @@
 public class A3Q7 {
 
     public static void countingSort(int[] n){
-        
+        int[] tracker = new int[101];
+        for(int i = 0; i < n.length; i++){
+            int value = n[i];
+            tracker[value]++;
+        }
+        int count = 0;
+        for(int i = 0; i < tracker.length; i++){
+            for(int a = 0; tracker[i] > a; a++){
+                n[count] = i;
+                count++;
+            }
+        }
     }
     /**
      * @param args the command line arguments
@@ -19,9 +30,13 @@ public class A3Q7 {
         // TODO code application logic here
         A3Q7 test = new A3Q7();
         int[] n = new int[4];
-        n[0] = 5;
+        n[0] = 3;
         n[1] = 3;
         n[2] = 8;
         n[3] = 10;
+        test.countingSort(n);
+        for(int i = 0; i < n.length; i++){
+            System.out.println(n[i]);
+        }
     }
 }
