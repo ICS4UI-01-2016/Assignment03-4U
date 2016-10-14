7ef108a879a05c4762d270052d9152eef1464af2
guanv6321 <guanv6321@HRH-IC0027321.wrdsb.ca>
2016-10-14 14:15:37 -0400

    q8

---
+++ src/A3Q8.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guanv6321
+ */
+public class A3Q8 {
+
+    public static void stringSort(String[] word){
+        for(int i=0; i<word.length; i++){
+            int j = i;
+            while (j>0 && word[j].compareTo(word[j-1])
+        }
+    } 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A3Q8 test = new A3Q8();
+        String[] word = {"", "", ""};
+        test.stringSort(word);
+    }
+}
