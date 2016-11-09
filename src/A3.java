e096060f56cdc7721fb0df933e85a2bb8febf4a7
moore3607 <moore3607@HRH-IC0027346.wrdsb.ca>
2016-11-09 13:19:03 -0500

    almost dunzo (working on question 8)

--- src/A3.java
+++ src/A3.java
@@ -33,9 +33,32 @@ public class A3 {
     }
     //created a method called "counting" that takes in one array (h)
     public void Counting(int[] h){
-        int[] e = new int [11];
+        //created a new array that contains 101 spots
+        int[] e = new int [101];
         for (int q = 0; q < h.length; q++){
             e[h[q]] = e[h[q]] + 1;
+        }
+        int spot = 0;
+        for(int a = 0; a < e.length; a++){
+            if(e[a] >= 1){
+                for(int z = 0; z < e[a]; z++){
+                    h[spot] = a;
+                    spot++;
+                }
+            }
+        }
+    }
+    //created a method that takes in one array of strings (words)
+    public void Alphabet(String[] w){
+        for(int i = 0; i < w.length; i++){
+            //checks to see if one word should be in front of another and 
+            //switches the locations in the array if necessary
+            if(w[i].compareToIgnoreCase(w[i+1]) > 0){
+                String temp = w[i];
+                w[i] = w[i+1];
+                w[i+1] = temp;
+            }
+            
         }
     }
 
@@ -43,14 +66,14 @@ public class A3 {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
         A3 test = new A3();
-        int[] n = {2, 1, 5, 8, 1, 7, 10, 6};
-        test.Insertion(n);
-
-        for (int i = 0; i < n.length; i++) {
-            System.out.println(n[i]);
-        }
+//        int[] n = {2, 1, 5, 8, 1, 7, 10, 6};
+//        test.Insertion(n);
+//
+//        for (int i = 0; i < n.length; i++) {
+//            System.out.println(n[i]);
+//        }
         
-        int[] h = new int[10];
+        int[] h = new int[50];
         for (int p = 0; p < h.length; p++){
             h[p] = (int)(Math.random()*(101));   
         }
@@ -58,6 +81,12 @@ public class A3 {
         
         for(int b = 0; b < h.length; b++){
             System.out.println(h[b]);
+        }
+        
+        String[] w = new String[3];
+        System.out.println("Enter " + w.length + " words please.");
+        for(int g = 0; g < w.length; g++){
+            w[g] = input.nextLine();
         }
         
 
