72a41d82ec08e2e81d6a883510c2e4fd9744a63e
moraj0721 <moraj0721@HRH-IC0027327.wrdsb.ca>
2016-11-14 13:37:09 -0500

    Buttons F=finished. Variable names for buttons are {Direction} (capitalized) + Button (capitalized).

---
+++ src/A3Q6.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author moraj0721
+ */
+public class A3Q6 {
+
+    public void insertionSort(int[] n){
+   for(int i = 1; i < n.length; i++)
+   {
+      int j = i;
+      while(j>0 && n[j-1] < n[j])
+      {
+         int temp = n[j];
+         n[j] = n[j-1];
+         n[j-1] = temp;
+         j--;
+      }
+   }
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Make the array 
+        A3Q6 sort = new A3Q6();
+        int[] n = {5, 6, 34, 29, 34, 87, 1, 9};
+        sort.insertionSort(n);
+        for(int i = 0; i < n.length; i++){
+             System.out.println(n[i]);
+        }
+       
+}
+}
