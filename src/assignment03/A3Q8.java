fa37ff8b34d1fbd64674ba236f5653562683790c
fostp4040 <fostp4040@HRH-IC0027333.wrdsb.ca>
2016-11-29 12:27:06 -0500

    Wrote a version of insertion sort for Strings

--- src/assignment03/A3Q8.java
+++ src/assignment03/A3Q8.java
@@ -13,26 +13,26 @@ public class A3Q8 {
     /**
      * @param args the command line arguments
      */
-    public int compareTo(String[] words){
-        for(int i = 0; i < words.length; i++)
-        {
+    
+    public void stringSort(String[] n){
+        for(int i = 1; i < n.length; i++){
             int j = i;
-            while(j>0 && this.char < words[j])
-            {
-                String temp = words[j];
-                words[j] = words[j-1];
-                words[j-1] = temp;
+            int string1= n[j].compareTo(n[j-1]);
+            int string2= n[j-1].compareTo(n[j]);
+            while(j>0 && string1 > string2){
+                String temp = n[j];
+                n[j] = n[j-1];
+                n[j-1] = temp;
                 j--;
             }
         }
-        return 0;
     }
     
     public static void main(String[] args) {
         // TODO code application logic here
         A3Q8 test = new A3Q8();
         String[] words = {"apple","corn","bee","pop"};
-        test.compareTo(words);
+        test.stringSort(words);
         for(int i = 0; i < words.length; i++){
             System.out.println(words[i]);
         }
